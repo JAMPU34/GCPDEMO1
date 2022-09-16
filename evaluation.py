7a0fee7e3ff2aff901e461817a08e79336aa2612
JAMPU34 <narayana2344@gmail.com>
2022-09-16 11:00:51 +0530

    working

--- evaluation.py
+++ evaluation.py
@@ -1,16 +1,16 @@
 from kfp.components import OutputPath,InputPath
 from typing import NamedTuple
 
-def evaluate(input_dir_path:InputPath()):
+def evaluate(input_dir_path:InputPath(),input_dir_path1:InputPath()):
     import joblib
     import os
     import pandas as pd
     from sklearn.metrics import mean_absolute_error
 
     model=joblib.load(os.path.join(input_dir_path,"model.joblib"))
-    test_X=pd.read_csv(os.path.join(input_dir_path, "test_X.csv"))
+    test_X=pd.read_csv(os.path.join(input_dir_path1, "test_X.csv"))
     test_X=test_X.to_numpy()
-    test_y=pd.read_csv(os.path.join(input_dir_path, "test_y.csv"))
+    test_y=pd.read_csv(os.path.join(input_dir_path1, "test_y.csv"))
     test_y=test_y.to_numpy()
 
     predictions = model.predict(test_X)
