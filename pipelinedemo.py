8c76ab4303af45225d049ec7c198b2b9c46beb1a
JAMPU34 <narayana2344@gmail.com>
2022-09-26 20:46:01 +0530

    Added endpoint, batchpredict and model registry

--- pipelinedemo.py
+++ pipelinedemo.py
@@ -4,19 +4,19 @@ from kfp import dsl
 from kfp.v2 import compiler
 from kfp import components
 import warnings
-from codetemplates import download_dataset,featureengineering,training,hypertuning,evaluation
+from codetemplates import download_dataset,featureengineering,training,hypertuning,evaluation,model_export,endpointdeploy,batchpredict
 
 experiment_name = 'test1'
 BASE_IMAGE = "python:3.8-slim"
 
 warnings.filterwarnings('ignore')
 
-pipeline_root="gs://lakshmiqwiklabs-gcp-03-8b866aa182e2aip-20220915080528/pipeline_root/intro"
+pipeline_root="gs://qwiklabs-gcp-04-9b56f269a5edaip-20220926060744/pipeline_root/intro"
 
 #Pipeline Orchastration
-@dsl.pipeline(name='lakshmipipeline8',description="A simple intro pipeline",pipeline_root=pipeline_root)
-def pipeline(query:str="""SELECT * FROM `qwiklabs-gcp-03-8b866aa182e2.lakshmi.lakshmidemo` LIMIT 10 """
-             ,datasetName:str="lakshmidemo.csv",project_id:str="qwiklabs-gcp-03-8b866aa182e2"):
+@dsl.pipeline(name='lakshmipipeline14',description="A simple intro pipeline",pipeline_root=pipeline_root)
+def pipeline(query:str="""SELECT * FROM `qwiklabs-gcp-04-9b56f269a5ed.lakshmi.lakshmidemo` LIMIT 10 """
+             ,datasetName:str="lakshmidemo.csv",project_id:str="qwiklabs-gcp-04-9b56f269a5ed"):
 
     #Components Creation
     download_dataset_opp=components.create_component_from_func(download_dataset.download_dataset,base_image=BASE_IMAGE,
@@ -24,21 +24,30 @@ def pipeline(query:str="""SELECT * FROM `qwiklabs-gcp-03-8b866aa182e2.lakshmi.la
     featureengineering_opp=components.create_component_from_func(featureengineering.featureengineering,base_image=BASE_IMAGE,
                                                     packages_to_install=['sklearn','pandas'])
     training_opp=components.create_component_from_func(training.training,base_image=BASE_IMAGE,
-                                                    packages_to_install=['sklearn','xgboost','pandas','joblib','numpy'])
+                                                    packages_to_install=['sklearn','xgboost','pandas','joblib','numpy','google-cloud-storage'])
 
     evaluation_opp=components.create_component_from_func(evaluation.evaluate,base_image=BASE_IMAGE,
                                                     packages_to_install=['sklearn','pandas','joblib','xgboost'])
 
     hypertuning_opp=components.create_component_from_func(hypertuning.hypertuning,base_image=BASE_IMAGE,
                                                     packages_to_install=[])
-
+    modelexport_opp=components.create_component_from_func(model_export.modelexport,base_image=BASE_IMAGE,
+                                                    packages_to_install=['google-cloud-aiplatform'])
+    endpointdeploy_opp=components.create_component_from_func(endpointdeploy.endpointdeploy,base_image=BASE_IMAGE,
+                                                    packages_to_install=['google-cloud-aiplatform'])
+    batchpredict_opp=components.create_component_from_func(batchpredict.batchpredict,base_image=BASE_IMAGE,
+                                                    packages_to_install=['google-cloud-aiplatform'])
+    
 
 
     #Building pipeline or stitching the components in order
     download_dataset_opp_final=download_dataset_opp(project_id=project_id,query=query)
     featureengineering_opp_final=featureengineering_opp(download_dataset_opp_final.output,datasetName)
-    training_opp_final=training_opp(featureengineering_opp_final.output)
+    training_opp_final=training_opp(featureengineering_opp_final.output,bucket_name="gs://qwiklabs-gcp-04-9b56f269a5edaip-20220926060744")
     evaluation_opp_final=evaluation_opp(training_opp_final.output,featureengineering_opp_final.output)
+    modelexport_opp_final=modelexport_opp(training_opp_final.output,project_id=project_id,bucket="gs://qwiklabs-gcp-04-9b56f269a5edaip-20220926060744")
+    endpointdeploy_opp_final=endpointdeploy_opp(modelexport_opp_final.output,project_id=project_id)
+    batchpredict_opp_final=batchpredict_opp(modelexport_opp_final.output,project_id=project_id)
 
 
 
@@ -62,7 +71,7 @@ def main(project_id2:str,staging_bucket:str,package_fileandpathname:str,display_
 
 
 if __name__ == '__main__':
-    main(project_id2="qwiklabs-gcp-03-8b866aa182e2",staging_bucket="gs://lakshmiqwiklabs-gcp-03-8b866aa182e2aip-20220915080528",package_fileandpathname="lakshmi.json",display_name="lakshmihelloworld",pipeline_root=pipeline_root)
+    main(project_id2="qwiklabs-gcp-04-9b56f269a5ed",staging_bucket="gs://qwiklabs-gcp-04-9b56f269a5edaip-20220926060744",package_fileandpathname="lakshmi.json",display_name="lakshmihelloworld",pipeline_root=pipeline_root)
 
 
 
